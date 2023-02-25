591dc424ab01f5411595f4ae65919208ddb31cf1
jay-java <jayprajapati.tops@gmail.com>
2023-02-25 19:51:12 +0530

    variable datatype condition

---
+++ src/main/kotlin/OperatorsDemo.kt
@@ -0,0 +1,96 @@
+fun main() {
+    //WHEN
+    var a = 20
+    var res:Int =  when(a){
+        10 -> 10
+        20 -> 20
+        30 -> 30
+        else -> 0
+    }
+    println(res)
+
+//    if(a == 10){
+//        print(10)
+//    }
+//    else if(a == 20){
+//        println(20)
+//    }
+//    else if(a ==30){
+//        println(a)
+//    }
+//    else{
+//        println()
+//    }
+
+
+
+
+//    var a =250
+//    var res = a in 100..250
+//    println(res)
+//
+//    var res1 = a in 100 until 250
+//    println(res1)
+
+//    var i =5
+//
+//    var res1 = i in 6..10
+//    println(res1)
+//    var res = i in 1 until 5
+//    println(res)
+//
+//    var a = 'A'
+//    var r = a in 'a'..'z'
+//    println(r)
+
+//    var i = 10
+//    var j =20
+////    var res = i == 11 && j ==21
+//    var res = i == 11 || j ==20
+//    println(res)
+
+//    var i=10
+//    var j = 20
+//    var res =  i == 11 || j == 22
+//    println(res)
+
+
+
+//    var i = 10
+//    println(i)
+//     var j = i++
+//    println(j)
+//
+//
+//
+//    var a = 10
+//    var b = 20
+//    var c = a+b
+//    println("addition of a and b = $c")
+//    println(a-b)
+//    println(a.toFloat()/b.toFloat())
+//    println(a*b)
+//    println(a%b)
+//
+//    var d = if(a>b){
+//        "a"
+//    }
+//    else {
+//        "b"
+//    }
+//    println("d = $d")
+
+//    if(a>b){
+//        println("a")
+//    }
+//    else{
+//        println("b")
+//    }
+
+//    println(a>b)
+//    println(a<b)
+//    println(a>=b)
+//    println(a<=b)
+//    println(a==b)
+//    println(a!=b)
+}
---
+++ src/main/kotlin/first.kt
@@ -0,0 +1,35 @@
+fun main() {
+
+    var a1= 10
+    a1 = 11
+    println(a1)
+
+    val b = 12
+    println(b)
+
+//    var a:Int = 12
+//    var b:String = "hello"
+//    var c:Char = 'r'
+//    var d:Float = 34.3f
+//    var e:Double = 34.3
+//    var f:Boolean = true
+
+//    println("hello kotlin")
+//    println("hello kotlin again")
+//    var a = 98
+
+//    println(a)
+//    a = 3
+//    println(a)
+//
+//    var i:Int = 234
+//    println(i)
+//
+//    val b = 20
+//    println(b)
+
+//    var b = "hello"
+//    println(b)
+
+
+}
