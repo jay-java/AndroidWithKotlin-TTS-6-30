b10087093a63a4b68a182addb699b90a0df1f185
jay-java <jayprajapati.tops@gmail.com>
2023-05-30 19:30:50 +0530

    sqlite db

---
+++ SQLITEDB/app/src/main/java/com/example/sqlitedb/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.sqlitedb
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sqlitedb.Adapter.StudentListAdapter
+import com.example.sqlitedb.Model.StudentModel
+import com.example.sqlitedb.database.DatabaseHelper
+
+class MainActivity : AppCompatActivity() {
+    lateinit var addBTN:Button
+    lateinit var recyclerView:RecyclerView
+    var adapter:StudentListAdapter? = null
+    var dbHandler:DatabaseHelper? = null
+    var slit : List<StudentModel> = ArrayList<StudentModel>()
+    var linearLayoutManager:LinearLayoutManager? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        addBTN = findViewById(R.id.add_Student_btn)
+        recyclerView  = findViewById(R.id.recycler_view_id)
+        dbHandler = DatabaseHelper(this)
+        fetchList()
+        addBTN.setOnClickListener {
+            val i = Intent(this@MainActivity,add_student::class.java)
+            startActivity(i)
+        }
+    }
+    private fun fetchList(){
+        slit = dbHandler!!.getAllStudents()
+        adapter = StudentListAdapter(slit,applicationContext)
+        linearLayoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.layoutManager = linearLayoutManager
+        recyclerView.adapter = adapter
+        adapter?.notifyDataSetChanged()
+    }
+}
---
+++ SQLITEDB/app/src/main/java/com/example/sqlitedb/Model/StudentModel.kt
@@ -0,0 +1,11 @@
+package com.example.sqlitedb.Model
+
+class StudentModel {
+    var id : Int = 0
+    var name :String = ""
+    var details:String = ""
+    override fun toString(): String {
+        return "StudentModel(id=$id, name='$name', details='$details')"
+    }
+
+}
